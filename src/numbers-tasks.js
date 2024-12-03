fa926e5df3e1251b5983369ab8a7c37e657a598a
dyoncha <dyoncha77@gmail.com>
2024-12-03 16:37:38 +0300

    feat: add getFibonacciNumber function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -254,10 +254,21 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
-}
+function getFibonacciNumber(index) {
+  if (index <= 1) {
+    return index;
+  }
 
+  let a = 0;
+  let b = 1;
+  for (let i = 2; i <= index; i += 1) {
+    const temp = a + b;
+    a = b;
+    b = temp;
+  }
+
+  return b;
+}
 /**
  * Returns the sum of all numbers from 1 to n.
  *
